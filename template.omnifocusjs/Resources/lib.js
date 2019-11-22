8226f4bd7b65ba529afa5cb87c1b645eb2e77168
Paul Sidnell <psidnell@yahoo.co.uk>
2019-11-22 22:26:49 +0000

    Template functions moved to lib

--- template.omnifocusjs/Resources/lib.js
+++ template.omnifocusjs/Resources/lib.js
@@ -1,35 +1,79 @@
 var _ = function() {
 	var lib = new PlugIn.Library(new Version("0.1"));
 
-	lib.createDeactivatedTag = () => {
-
-		var hiddenTagName = "HIDDEN";
-		var hiddenTagGroup = tagNamed(hiddenTagName) || new Tag(hiddenTagName);
-
-		var deactivatedTagName = "DEACTIVATED";
-		var deactivatedTag = hiddenTagGroup.tagNamed(deactivatedTagName) || new Tag(deactivatedTagName, hiddenTagGroup);
-
-		return deactivatedTag;
-	};
-
-	lib.toggle = function(child, deactivatedTag) {
-		if (child instanceof Project) {
-			if (child.status === Project.Status.OnHold
-				&& child.task.tags.includes(deactivatedTag)) {
-				child.task.removeTag(deactivatedTag);
-				child.status = Project.Status.Active;
-			} else if ( child.status === Project.Status.Active) {
-				child.task.addTag(deactivatedTag);
-				child.status = Project.Status.OnHold;
+	// Find variables in text (thanks to Thomas Kern)
+	lib.findVariables = (text, variablesFound) => {
+		substrings = text.match(/\$\{[^\}]*\}/gm);
+		if (substrings != null) {
+			for (var i = 0; i < substrings.length; i++) {
+				var variable = substrings[i];
+				if (!variablesFound.includes(variable)) {
+					console.log("Found variable " + variable);
+					variablesFound.push(variable);
+				}
 			}
 		}
 	};
 
-	lib.toggleFolderOrProject = function(item) {
-		var deactivatedTag = lib.createDeactivatedTag();
-		item.apply(child => {
-			lib.toggle(child, deactivatedTag);
+	// Find all the variables used in the template
+	lib.findVariablesInTemplate = (template) => {
+		// Find the variables used in the template
+		var templateVariables = [];
+		template.task.apply((task) => {
+			lib.findVariables(task.name, templateVariables);
+			lib.findVariables(task.note, templateVariables)
 		});
+
+		return templateVariables;
+	};
+
+	// Replace variables in the string with those from the form
+	lib.replaceVariables = (text, variables) => {
+		var result = text;
+		for (var variable in variables) {
+			var value = variables[variable];
+			result = result.replace(variable, value);
+		}
+
+		return result;
+	};
+
+	// Replace any variables in the task title or note
+	lib.replaceVariableInTask = (task, variables) => {
+		console.log('Processing Task ' + task.name);
+		task.name = lib.replaceVariables(task.name, variables);
+		task.note = lib.replaceVariables(task.note, variables);
+	};
+
+	// Process the template
+	lib.expandTemplate = (template, form) => {
+		console.log('Processing Template ' + template.name);
+		// Duplicate the template project
+		var project = duplicateSections([template], template.before)[0];
+
+		// Make it active
+		project.status = Project.Status.Active;
+
+		// Replace any variables in the project
+		project.task.apply((task) => {
+			lib.replaceVariableInTask(task, form.values);
+		})
+	};
+
+	lib.expand = (template) => {
+		var templateVariables = lib.findVariablesInTemplate(template);
+
+		// Open a form to collect values for variables in the template
+		inputForm = new Form();
+		for (var i = 0; i < templateVariables.length; i++) {
+			var variable = templateVariables[i];
+			inputForm.addField(new Form.Field.String(variable, variable, null), i);
+		}
+		formPrompt = "Provide values for the template";
+		buttonTitle = "OK";
+		inputForm.show(formPrompt, buttonTitle).then(
+			(form) => lib.expandTemplate(template, form),
+			(error) => console.log(error));
 	};
 
 	return lib;
--- template.omnifocusjs/Resources/template.js
+++ template.omnifocusjs/Resources/template.js
@@ -1,52 +1,7 @@
 var _ = (function() {
     var action = new PlugIn.Action((selection, sender) => {
 
-        // Find variables in text (thanks to Thomas Kern)
-        var findVariables = function(text, variablesFound) {
-            substrings = text.match(/\$\{[^\}]*\}/gm);
-            if (substrings != null) {
-                for (var i = 0; i < substrings.length; i++) {
-                    var variable = substrings[i];
-                    if (!variablesFound.includes(variable)) {
-                        console.log("Found variable " + variable);
-                        variablesFound.push(variable);
-                    }
-                }
-            }
-        };
-
-        // Replace variables in the string with those from the form
-        var replaceVariables = function(text, variables) {
-            var result = text;
-            for (var variable in variables) {
-                var value = variables[variable];
-                result = result.replace(variable, value);
-            }
-
-            return result;
-        };
-
-        // Replace any variables in the task title or note
-        var replaceVariableInTask = function(task, variables) {
-            console.log('Processing Task ' + task.name);
-            task.name = replaceVariables(task.name, variables);
-            task.note = replaceVariables(task.note, variables);
-        };
-
-        // Process the template
-        var process = function(template, form) {
-            console.log('Processing Template ' + template.name);
-            // Duplicate the template project
-            var project = duplicateSections([template], template.before)[0];
-
-            // Make it active
-            project.status = Project.Status.Active;
-
-            // Replace any variables in the project
-            project.task.apply((task) => {
-                replaceVariableInTask(task, form.values);
-            })
-        };
+        var lib = PlugIn.find("com.PaulSidnell.Template").library("lib");
 
         // The selected project is the template to be used
         // TODO
@@ -55,26 +10,8 @@ var _ = (function() {
         var template = folderNamed('Work')
             .folderNamed('WebView')
             .projectNamed('${Ticket} - ${Description}');
-        console.log('Template project is ' + template);
 
-        // Find the variables used in the template
-        var templateVariables = [];
-        template.task.apply((task) => {
-            findVariables(task.name, templateVariables);
-            findVariables(task.note, templateVariables)
-        });
-
-        // Open a form to collect values for variables in the template
-        inputForm = new Form();
-        for (var i = 0; i < templateVariables.length; i++) {
-            var variable = templateVariables[i];
-            inputForm.addField(new Form.Field.String(variable, variable, null), i);
-        }
-        formPrompt = "Provide values for the template";
-        buttonTitle = "OK";
-        inputForm.show(formPrompt, buttonTitle).then(
-            (form) => process(template, form),
-            (error) => console.log(error));
+        lib.expand(template);
     });
 
     action.validate = (selection, sender) => {
