7bd57e5b3ee91c5c077bd1e93bebe231e5de818a
Paul Sidnell <paul.sidnell@zellis.com>
2019-11-22 14:24:49 +0000

    Change result of variable finding

--- test.spec.js
+++ test.spec.js
@@ -1,5 +1,21 @@
-describe('Test', () => {
-    it('can run a test', () => {
-        expect(1).toBe(2);
+var findVariables = function(text, variablesFound) {
+    substrings = text.match(/\$\{[^\}]*\}/gm);
+    if (substrings != null) {
+        for (var i = 0; i < substrings.length; i++) {
+            var variable = substrings[i];
+            if (!variablesFound.includes(variable))
+                variablesFound.push(variable);
+        }
+    }
+};
+
+describe('Can find variables', () => {
+    it('can find variable', () => {
+        var text = "this is a ${variable1} ${variable2}";
+        let variablesFound = [];
+        findVariables(text, variablesFound);
+        expect(variablesFound[0]).toEqual("${variable1}");
+        expect(variablesFound[1]).toEqual("${variable2}");
+        expect(variablesFound.length).toBe(2);
     });
-})
+});
