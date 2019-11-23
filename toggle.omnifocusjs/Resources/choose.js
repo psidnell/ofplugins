762d119b01a19364ecae5b778e7733c27b0db21d
Paul Sidnell <psidnell@yahoo.co.uk>
2019-11-23 21:17:55 +0000

    Added choice to toggle

---
+++ toggle.omnifocusjs/Resources/choose.js
@@ -0,0 +1,42 @@
+var _ = (function() {
+    var action = new PlugIn.Action((selection, sender) => {
+
+        var toggleLib = PlugIn.find("com.PaulSidnell.Toggle").library("lib");
+
+        var inputForm = new Form();
+
+        // Find folders
+        var folders = [];
+        var names = [];
+        library.apply(item => {
+            if (item instanceof Folder) {
+                folders.push(item);
+                names.push(item.name);
+            }
+        });
+
+        if (folders.length > 0) {
+            // Choose which folder to toggle
+            inputForm.addField(new Form.Field.Option(
+                "choice",
+                "Position",
+                folders,
+                names,
+                folders[0]
+            ));
+
+            var formPrompt = "Choose a folder";
+            var buttonTitle = "OK";
+            inputForm.show(formPrompt, buttonTitle).then(
+                (form) => toggleLib.toggleFolder(form.values['choice']),
+                (error) => console.log(error));
+        }
+    });
+
+    action.validate = (selection, sender) => {
+        return true;
+    };
+
+    return action;
+})();
+_;
