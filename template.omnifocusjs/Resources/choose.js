41d2ba79272c0399c3ca77881ec8509d9b8c3841
Paul Sidnell <psidnell@yahoo.co.uk>
2019-11-23 21:04:40 +0000

    Template provides a selection option

---
+++ template.omnifocusjs/Resources/choose.js
@@ -0,0 +1,42 @@
+var _ = (function() {
+    var action = new PlugIn.Action((selection, sender) => {
+
+        var lib = PlugIn.find("com.PaulSidnell.Template").library("lib");
+
+        var inputForm = new Form();
+
+        // Find templates
+        var templates = [];
+        var names = [];
+        library.apply(item => {
+            if (item instanceof Project && item.name.indexOf('${') != -1) {
+                templates.push(item);
+                names.push(item.name);
+            }
+        });
+
+        if (templates.length > 0) {
+            // Choose which template to expand
+            inputForm.addField(new Form.Field.Option(
+                "choice",
+                "Position",
+                templates,
+                names,
+                templates[0]
+            ));
+
+            var formPrompt = "Choose a template";
+            var buttonTitle = "OK";
+            inputForm.show(formPrompt, buttonTitle).then(
+                (form) => lib.expand(form.values['choice']),
+                (error) => console.log(error));
+        }
+    });
+
+    action.validate = (selection, sender) => {
+        return true;
+    };
+
+    return action;
+})();
+_;
