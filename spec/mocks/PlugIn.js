d87ddd3738a1e17784dafc44ad69b4cf3f923769
Paul Sidnell <paul.sidnell@zellis.com>
2019-11-25 15:39:48 +0000

    Testing

---
+++ spec/mocks/PlugIn.js
@@ -0,0 +1,7 @@
+class PlugIn {
+    print () {
+        console.log("Hello");
+    };
+}
+
+module.exports = PlugIn;
---
+++ spec/toggle.omnifocus.js/Resources/chooseSpec.js
@@ -0,0 +1,9 @@
+describe('choose', () => {
+    var PlugIn = require('../../mocks/PlugIn');
+    //require('./choose');
+
+    it('can test code', () => {
+        var demo = new PlugIn();
+        demo.print();
+    });
+});
