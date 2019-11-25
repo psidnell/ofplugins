9656dad94fd6ae92c2e2f05e9dbaa01badb2b9f5
Paul Sidnell <paul.sidnell@zellis.com>
2019-11-25 17:06:58 +0000

    More tests

--- spec/toggle.omnifocus.js/Resources/chooseSpec.js
+++ spec/toggle.omnifocus.js/Resources/chooseSpec.js
@@ -4,6 +4,8 @@ describe('choose', () => {
 
     beforeEach(function() {
         require('../../mocks/PlugIn');
+        require('../../mocks/library');
+        require('../../mocks/Form');
         require('../../../toggle.omnifocusjs/Resources/choose.js');
         action = lastPlugInAction;
     });
@@ -12,5 +14,25 @@ describe('choose', () => {
         expect(action).toBeTruthy();
         expect(action.fn).toBeTruthy();
         expect(action.validate).toBeTruthy();
+    });
+
+    it('can validate', () => {
+        expect(action.validate(null, null)).toBeTruthy();
+    });
+
+    it('can run when no folders', () => {
+        var plugin = {}
+        var toggleLib = {};
+
+        // Expectations
+        library.apply = jasmine.createSpy('apply');
+        PlugIn.find = jasmine.createSpy('find').and.returnValue(plugin);
+        plugin.library = jasmine.createSpy('library').and.returnValue(toggleLib);
+
+        // Test
+        action.fn(null, null);
+
+        // Verify
+        expect(library.apply).toHaveBeenCalled();
     });
 });
