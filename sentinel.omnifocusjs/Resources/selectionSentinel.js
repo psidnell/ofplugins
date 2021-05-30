330e098bbf8306f715e451167dafd4d08a0927d0
Paul Sidnell <psidnell@yahoo.co.uk>
2021-05-30 12:18:16 +0100

    Sentinel task - also supports selection of tags

--- sentinel.omnifocusjs/Resources/selectionSentinel.js
+++ sentinel.omnifocusjs/Resources/selectionSentinel.js
@@ -1,33 +1,46 @@
 var _ = (function() {
     var action = new PlugIn.Action((selection, sender) => {
 
-        var task = selection.tasks[0];
+        var tags = [];
+
+        // Tags from any selected tasks
+        selection.tasks.forEach(task => {
+            task.tags.forEach(tag => {
+                if (!tags.includes(tag)) {
+                    tags.push(tag);
+                }
+            })
+        });
+
+        // Any selected tags
+        selection.tags.forEach(tag => {
+            if (!tags.includes(tag)) {
+                tags.push(tag);
+            }
+        });
 
         var tagNameList = [];
-        task.tags.forEach((tag)=>{
+        tags.forEach((tag)=>{
             tagNameList.push(tag.name);
         });
         var title = '➡️ ' + tagNameList.join(', ');
 
         var noteLines = [];
-        task.tags.forEach((tag)=>{
+        tags.forEach((tag)=>{
             noteLines.push('omnifocus:///tag/' +  encodeURIComponent(tag.name));
         });
         var note = noteLines.join('\n');
 
-        var taskCopy = duplicateTasks([task], task.containingProject)[0];
-        taskCopy.name = title;
-        taskCopy.repetitionRule = null;
-        taskCopy.dueDate = null;
-        taskCopy.addTag(Tag.forecastTag);
-        taskCopy.note = note;
+        var task = new Task(title, inbox.beginning);
+        task.addTag(Tag.forecastTag);
+        tags.forEach(tag => {
+            task.addTag(tag);
+        });
+        task.note = note;
     });
 
     action.validate = (selection, sender) => {
-        return selection.tasks
-            && selection.tasks.length == 1
-            && selection.tasks[0].containingProject != null
-            && Tag.forecastTag != null
+        return selection.tasks.length >= 1 || selection.tags.length >= 1;
     };
 
     return action;
