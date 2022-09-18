6ec03eaf0094fb62178a60cd911c5205b89fa98a
Paul Sidnell <psidnell@nowhere.com>
2022-09-18 18:46:37 +0100

    Logging

--- draftstemplate.omnifocusjs/Resources/draftstemplate.js
+++ draftstemplate.omnifocusjs/Resources/draftstemplate.js
@@ -1,16 +1,18 @@
 var _ = (function() {
 
 	const action = new PlugIn.Action(async function(selection, sender){
+	    console.log('action running');
 		const fileTypeID = "com.omnigroup.omnifocus2.export-filetype.plain-text";
-		const baseName = "Tasky-Export";
-		const wrapper = await document.makeFileWrapper(baseName, fileTypeID);
+		const wrapper = await document.makeFileWrapper("Tasky-Export", fileTypeID);
+		console.log('Wrapper fetched');
 		const taskPaper = wrapper.contents.toString()
 		const urlStr = "drafts://x-callback-url/" +
 			"create" +
 			"?action=Project%20from%20Template" +
 			"&text=" + encodeURIComponent(taskPaper);
+        console.log('url: ',urlStr);
 		const scriptURL = URL.fromString(urlStr);
-        console.log(scriptURL);
+        console.log('scriptURL: ' +  scriptURL);
         scriptURL.open();
 	});
 
