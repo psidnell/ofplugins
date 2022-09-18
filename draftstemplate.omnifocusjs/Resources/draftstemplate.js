8752a9065a82b8f01b0dbe481aeaefbd085503be
Paul Sidnell <psidnell@nowhere.com>
2022-09-18 18:54:31 +0100

    Reverting to original promise code that worked on iOS

--- draftstemplate.omnifocusjs/Resources/draftstemplate.js
+++ draftstemplate.omnifocusjs/Resources/draftstemplate.js
@@ -1,19 +1,21 @@
 var _ = (function() {
 
-	const action = new PlugIn.Action(async function(selection, sender){
+	const action = new PlugIn.Action(function(selection, sender){
 	    console.log('action running');
 		const fileTypeID = "com.omnigroup.omnifocus2.export-filetype.plain-text";
-		const wrapper = await document.makeFileWrapper("Tasky-Export", fileTypeID);
-		console.log('Wrapper fetched');
-		const taskPaper = wrapper.contents.toString()
-		const urlStr = "drafts://x-callback-url/" +
-			"create" +
-			"?action=Project%20from%20Template" +
-			"&text=" + encodeURIComponent(taskPaper);
-        console.log('url: ',urlStr);
-		const scriptURL = URL.fromString(urlStr);
-        console.log('scriptURL: ' +  scriptURL);
-        scriptURL.open();
+		const wrapperPromise = document.makeFileWrapper("Tasky-Export", fileTypeID);
+		wrapperPromise.then(wrapper => {
+            console.log('Wrapper fetched');
+            const taskPaper = wrapper.contents.toString()
+            const urlStr = "drafts://x-callback-url/" +
+                "create" +
+                "?action=Project%20from%20Template" +
+                "&text=" + encodeURIComponent(taskPaper);
+            console.log('url: ',urlStr);
+            const scriptURL = URL.fromString(urlStr);
+            console.log('scriptURL: ' +  scriptURL);
+            scriptURL.open();
+        });
 	});
 
 	action.validate = function(selection, sender){
