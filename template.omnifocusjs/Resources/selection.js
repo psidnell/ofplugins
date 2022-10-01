4760e4cfa23b5be664d2471d143da7df8de098ef
Paul Sidnell <psidnell@nowhere.com>
2022-10-01 16:51:39 +0100

    Fix for OF4 on iOS TestFlight

--- template.omnifocusjs/Resources/selection.js
+++ template.omnifocusjs/Resources/selection.js
@@ -2,10 +2,26 @@ var _ = (function() {
 
     const action = new PlugIn.Action(async function(selection, sender){
 
-        const fileTypeID = "com.omnigroup.omnifocus2.export-filetype.plain-text"
-        const baseName = "Tasky-Export"
-        const wrapper = await document.makeFileWrapper(baseName, fileTypeID);
-        const taskPaper = wrapper.contents.toString()
+         // Should contain com.omnigroup.omnifocus2.export-filetype.plain-text
+        // But doesn't in OF4 on iOS
+        var types = document.writableTypes.map(type => {
+            return "\"" + type + "\""
+        })
+        types = "[" + types.join(",\n") + "]";
+        // console.log("Supported file types: " + types);
+
+        // Where supported fetch the taskpaper of the selected project
+        // otherwise (e.g. the test flight of OF4 on iOS) you must copy
+        // the project as taskpsper
+        var taskPaper;
+        if (types.indexOf("com.omnigroup.omnifocus2.export-filetype.plain-text") != -1) {
+            const fileTypeID = "com.omnigroup.omnifocus2.export-filetype.plain-text"
+            const baseName = "Tasky-Export"
+            const wrapper = await document.makeFileWrapper(baseName, fileTypeID);
+            taskPaper = wrapper.contents.toString();
+        } else {
+            taskPaper = Pasteboard.general.string;
+        }
 
         var lib = PlugIn.find("com.PaulSidnell.Template").library("lib");
 
