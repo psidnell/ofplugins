f39a50f9e8a141a2bf759ec9184692964dfca905
Paul Sidnell <psidnell@nowhere.com>
2022-10-03 08:53:58 +0100

    Preserve perspective when changing focus

--- focus-work.omnifocusjs/Resources/focus.js
+++ focus-work.omnifocusjs/Resources/focus.js
@@ -1,11 +1,16 @@
 var _ = (function() {
 
 	var action = new PlugIn.Action(function(selection, sender){
+
+        const SEARCH_FOR = "work";
+
+	    var startingPerspective = document.windows[0].perspective;
+
 	    var matches = [];
 		flattenedFolders.forEach(folder => {
 		    if ((!folder.parent) &&
 		        folder.status == Folder.Status.Active &&
-		        (folder.name.toLowerCase().indexOf("work") != -1 || folder.name.toLowerCase().indexOf("misc") != -1)) {
+		        (folder.name.toLowerCase().indexOf(SEARCH_FOR) != -1 || folder.name.toLowerCase().indexOf("misc") != -1)) {
 		        matches.push(folder);
 		        // console.log("Matched Folder " + folder.name);
             }
@@ -15,7 +20,7 @@ var _ = (function() {
             if ((!project.parentFolder) &&
                 project.status != Project.Status.Dropped &&
                 project.status != Project.Status.Done &&
-                (project.name.toLowerCase().indexOf("work") != -1 || project.name.toLowerCase().indexOf("misc") != -1)) {
+                (project.name.toLowerCase().indexOf(SEARCH_FOR) != -1 || project.name.toLowerCase().indexOf("misc") != -1)) {
                 matches.push(project);
                 // console.log("Matched Project " + project.name);
             }
@@ -24,6 +29,8 @@ var _ = (function() {
         var win = document.windows[0]
         win.perspective = Perspective.BuiltIn.Projects
         win.focus = matches;
+
+        document.windows[0].perspective = startingPerspective;
 	});
 
 	action.validate = function(selection, sender){
