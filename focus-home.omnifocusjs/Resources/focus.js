d7fb77abecbcdd55d156ec0d091b8ef2e5828770
Paul Sidnell <psidnell@nowhere.com>
2022-10-03 08:12:30 +0100

    Added work focus plugin

---
+++ focus-home.omnifocusjs/Resources/focus.js
@@ -0,0 +1,31 @@
+var _ = (function() {
+
+	var action = new PlugIn.Action(function(selection, sender){
+	    var matches = [];
+		flattenedFolders.forEach(folder => {
+		    if ((!folder.parent) &&
+		        folder.status == Folder.Status.Active &&
+		        (folder.name.toLowerCase().indexOf("work") != -1 || folder.name.toLowerCase().indexOf("misc") != -1)) {
+		        matches.push(folder);
+		        console.log("Matched Folder " + folder.name);
+            }
+		});
+
+		flattenedProjects.forEach(project => {
+        		    if ((!project.parentFolder) &&
+        		        project.status != Project.Status.Dropped &&
+        		        project.status != Project.Status.Done &&
+        		        (project.name.toLowerCase().indexOf("work") != -1 || project.name.toLowerCase().indexOf("misc") != -1)) {
+        		        matches.push(project);
+        		        console.log("Matched Project " + project.name);
+                    }
+        		});
+	});
+
+	action.validate = function(selection, sender){
+		return true;
+	};
+
+	return action;
+})();
+_;
