9e2c711fd9a9f629b6c2f9b1e1a8fe7f9be3dd4e
Paul Sidnell <psidnell@nowhere.com>
2022-12-17 08:49:08 +0000

    Repuposing hide alarm plugin as more general routine hiding

--- hide-routine.omnifocusjs/Resources/hide.js
+++ hide-routine.omnifocusjs/Resources/hide.js
@@ -1,16 +1,24 @@
 var _ = (function() {
 
+	const SEARCH_FOR = ["alarms", "routine", "templates"];
+
+	var matchesStrings = (name) => {
+		var nameLc = name.toLowerCase();
+		for (s of SEARCH_FOR) {
+			if (nameLc.indexOf(s) !== -1) {
+				return true;
+			}
+		}
+		return false;
+	}
 	var action = new PlugIn.Action(async function(selection, sender){
-
-        const SEARCH_FOR = "alarms";
-
 	    var startingPerspective = document.windows[0].perspective;
 
 	    var matches = [];
 		flattenedFolders.forEach(folder => {
 		    if ((!folder.parent) &&
 		        folder.status == Folder.Status.Active &&
-		        (folder.name.toLowerCase().indexOf(SEARCH_FOR) == -1)) {
+		        !matchesStrings(folder.name)) {
 		        matches.push(folder);
 		        // console.log("Matched Folder " + folder.name);
             }
@@ -20,7 +28,7 @@ var _ = (function() {
             if ((!project.parentFolder) &&
                 project.status != Project.Status.Dropped &&
                 project.status != Project.Status.Done &&
-                (project.name.toLowerCase().indexOf(SEARCH_FOR) == -1)) {
+				!matchesStrings(project.name)) {
                 matches.push(project);
                 // console.log("Matched Project " + project.name);
             }
