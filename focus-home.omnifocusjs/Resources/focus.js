1f88572a64297fa16195e689951a874a19a79310
Paul Sidnell <psidnell@nowhere.com>
2023-07-08 08:12:44 +0100

    Changed focus to exclude rather than include

--- focus-home.omnifocusjs/Resources/focus.js
+++ focus-home.omnifocusjs/Resources/focus.js
@@ -2,7 +2,7 @@ var _ = (function() {
 
 	var action = new PlugIn.Action(async function(selection, sender){
 
-        const SEARCH_FOR = "home";
+        const EXCLUDE = "work";
 
 	    var startingPerspective = document.windows[0].perspective;
 
@@ -10,7 +10,7 @@ var _ = (function() {
 		flattenedFolders.forEach(folder => {
 		    if ((!folder.parent) &&
 		        folder.status == Folder.Status.Active &&
-		        (folder.name.toLowerCase().indexOf(SEARCH_FOR) != -1 || folder.name.toLowerCase().indexOf("misc") != -1)) {
+		        (folder.name.toLowerCase().indexOf(EXCLUDE) == -1)) {
 		        matches.push(folder);
 		        // console.log("Matched Folder " + folder.name);
             }
@@ -20,7 +20,7 @@ var _ = (function() {
             if ((!project.parentFolder) &&
                 project.status != Project.Status.Dropped &&
                 project.status != Project.Status.Done &&
-                (project.name.toLowerCase().indexOf(SEARCH_FOR) != -1 || project.name.toLowerCase().indexOf("misc") != -1)) {
+                (project.name.toLowerCase().indexOf(EXCLUDE) == -1)) {
                 matches.push(project);
                 // console.log("Matched Project " + project.name);
             }
--- focus-work.omnifocusjs/Resources/focus.js
+++ focus-work.omnifocusjs/Resources/focus.js
@@ -2,29 +2,29 @@ var _ = (function() {
 
 	var action = new PlugIn.Action(async function(selection, sender){
 
-        const SEARCH_FOR = "work";
+        const EXCLUDE = "home";
 
 	    var startingPerspective = document.windows[0].perspective;
 
 	    var matches = [];
 		flattenedFolders.forEach(folder => {
-		    if ((!folder.parent) &&
-		        folder.status == Folder.Status.Active &&
-		        (folder.name.toLowerCase().indexOf(SEARCH_FOR) != -1 || folder.name.toLowerCase().indexOf("misc") != -1)) {
-		        matches.push(folder);
-		        // console.log("Matched Folder " + folder.name);
-            }
+			if ((!folder.parent) &&
+				folder.status == Folder.Status.Active &&
+				(folder.name.toLowerCase().indexOf(EXCLUDE) == -1)) {
+				matches.push(folder);
+				// console.log("Matched Folder " + folder.name);
+			}
 		});
 
 		flattenedProjects.forEach(project => {
-            if ((!project.parentFolder) &&
-                project.status != Project.Status.Dropped &&
-                project.status != Project.Status.Done &&
-                (project.name.toLowerCase().indexOf(SEARCH_FOR) != -1 || project.name.toLowerCase().indexOf("misc") != -1)) {
-                matches.push(project);
-                // console.log("Matched Project " + project.name);
-            }
-        });
+			if ((!project.parentFolder) &&
+				project.status != Project.Status.Dropped &&
+				project.status != Project.Status.Done &&
+				(project.name.toLowerCase().indexOf(EXCLUDE) == -1)) {
+				matches.push(project);
+				// console.log("Matched Project " + project.name);
+			}
+		});
 
         var win = document.windows[0]
         win.perspective = Perspective.BuiltIn.Projects
